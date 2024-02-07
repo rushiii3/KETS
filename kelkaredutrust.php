6053e968869af8151bed315f111a63e4d7902d24
Vipulbhoir04 <141430394+Vipulbhoir04@users.noreply.github.com>
2024-02-07 17:08:34 +0530

    Add files via upload
    education trust page

---
+++ kelkaredutrust.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelkar Education Trust</title>
+    <?php include('../../library/library.php'); ?>
+</head>
+
+<body>
+    <?php include('../Layouts/header.php'); ?>
+
+    <!-- component -->
+    <main class="mt-2">
+        <div class=" container py-5  flex justify-between px-4 mx-auto ">
+            <article
+                class="w-full   px-4 rounded-lg    mx-auto format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                <div class="relative py-2 lg:py-16 pt-0 lg:pt-2">
+                    <div class="max-w-8xl   mx-auto  ">
+                        <div class="text-center p-8 mt-3">
+                            <h2 class="text-blue-900 font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+                                data-aos="flip-up">
+                                Kelkar Education Trust
+                            </h2>
+                        </div>
+                    </div>
+                    <h2
+                        class=" mb-0 lg:mb-1 font-sans text-lg lg:text-3xl text-center lg:text-left font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
+                        <span class="relative text-xl lg:text-3xl text-left" data-aos="fade-right">
+                            Kelkar Education Trust does not take any donations for admissions.
+                        </span>
+                    </h2>
+                    <h2>
+                        <p class="pt-2 mb-3"> The Late Shri Annasaheb Vaze and his sons established the Kelkar Education
+                            Trust in 1979. This charitable Trust aims to reach out to all, irrespective of their
+                            standing in society, without any discrimination. Any voluntary donation to the Trust, is
+                            eligible for exemption under Section 80-G of the Income Tax Act, 1961.</p>
+                    </h2>
+
+                    <h2
+                        class=" mb-0 lg:mb-1 font-sans text-lg lg:text-3xl text-center lg:text-left font-bold leading-none tracking-tight text-gray-900  md:mx-auto">
+                        <span class="relative text-xl lg:text-3xl text-center " data-aos="fade-right">
+                            Objectives of the Trust:
+                        </span>
+                    </h2>
+                    <ul class="list-disc list-inside ml-3">
+                        <p class="pt-2 mb-2 ">
+                            <li>In order to bring education within the reach of all, the KET</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Organizes seminars, conferences, conventions,</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Publishes and/or helps in publication of books, journals, etc.</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Provides relief to the underprivileged for their education,</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Encourages advancement of knowledge to the general public to further the educational
+                                goals of the nation,</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Liaises and/or affiliates with other colleges, institutions that have similar goals, and
+                                research interests,</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li><strong class="font-bold">Promotes Research and Development activities towards the
+                                    following fields:</strong>
+                                <ul class="list-disc list-inside ml-6">
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Plant Biotechnology in general and Aromatic and Medicinal plant
+                                            biotechnology in particular.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Field trials related research in Biotechnology.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Developing designs and pilot plant trials for utilization of research in
+                                            biotechnology.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Microbial and enzyme transformations.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Biotechnology based techniques for pollution control and environment
+                                            Protection.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Establishing protocols for plants of economic importance for rural
+                                            development, social forestry and horticulture.</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Biotechnology, Plant Tissue culture, Biochemistry, Chemistry, Botany,
+                                            Microbiology, Zoology, Chemical Engineering, Quality Management, Food
+                                            Preservation and other related areas,</li>
+                                    </p>
+                                    <p class="pt-2 mb-2 ">
+                                        <li>Viable technologies, which are of economic and national importance.</li>
+                                    </p>
+                                </ul>
+                            </li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Has established a Research and Development center, set up research laboratories equipped
+                                with latest research equipments and machinery, to carry out the objectives of the Trust.
+                            </li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Engages with scientific and other supporting non-scientific personnel</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Establishes and conducts Diploma courses in Perfumery and Cosmetics and also other
+                                courses permitted by the University of Mumbai and /or University Grants Commission or
+                                any other approved authority.</li>
+                        </p>
+                        <p class="pt-2 mb-2 ">
+                            <li>Provides relief to the underprivileged for their educationEngages with persons, acts and
+                                deeds that may be incidental, necessary or conductive to achieve the objectives of the
+                                Trust.</li>
+                        </p>
+                    </ul>
+
+                    <p class="pt-2 mb-3 mt-3">Keeping in view the Objectives of the Trust, the Late Shri Govind Damodar
+                        fondly referred to as Bhausaheb Kelkar established the V.G. Vaze College of Arts, Science and
+                        Commerce at Mithagar Road, Mulund (E) Mumbai-81 on 20th June 1984. He created well equipped
+                        laboratories for undergraduate courses and state of the art laboratories for research and
+                        development. The Scientific Research Center was subsequently established. The Trust promotes
+                        Research by encouraging the faculty to engage in research activities including higher degrees as
+                        also to guide students who wish to do M.Phils and PhDs. The Advanced Chemistry Laboratory and
+                        Instrumentation Center were added in due course of time to expand the scope of scientific
+                        activities</p>
+
+                    <p class="pt-2 mb-3 mt-3">Late Bhausaheb Kelkar, a renowned Industrialist and Philanthropist
+                        believed in the philosophy “No Business in Charity and No Charity in Business”.</p>
+
+                    <p class="pt-2 mb-3 mt-3">The present Trustees follow the path laid down by the visionaries, Late
+                        Annasaheb Vaze and Late Bhausaheb Kelkar.</p>
+                </div>
+            </article>
+        </div>
+    </main>
+
+    <?php include("../Layouts/footer.php"); ?>
+</body>
+<?php include("../../library/AOS.php"); ?>
+
+</html>
