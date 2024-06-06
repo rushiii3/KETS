b73e665dc4f3e2e292d83ffbf64411a94213b7e3
Nitin Singh <141268460+NitinSingh0@users.noreply.github.com>
2024-06-06 08:10:48 +0530

    image added

--- College/src/main/Components/research_home.php
+++ College/src/main/Components/research_home.php
@@ -1,6 +1,50 @@
+<!-- component -->
+<div className="w-1/2 text-left">
+  <nav aria-label="breadcrumb" class="w-max">
+    <ol class="flex w-full flex-wrap items-center rounded-md bg-blue-gray-50 bg-opacity-60 py-2 px-4">
+      <li class="flex cursor-pointer items-center font-sans text-sm font-normal leading-normal text-blue-gray-900 antialiased transition-colors duration-300 hover:text-blue-500">
+        <a class="opacity-60" href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+          </svg>
+        </a>
+        <span class="pointer-events-none mx-2 select-none font-sans text-sm font-normal leading-normal text-blue-gray-500 antialiased">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
+        </span>
+      </li>
+      <li class="flex cursor-pointer items-center font-sans text-sm font-normal leading-normal text-blue-gray-900 antialiased transition-colors duration-300 hover:text-blue-500">
+        <a class="opacity-60" href="#">
+          <span>Research</span>
+        </a>
+        <span class="pointer-events-none mx-2 select-none font-sans text-sm font-normal leading-normal text-blue-gray-500 antialiased">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>
+        </span>
+      </li>
+      <li class="flex cursor-pointer items-center font-sans text-sm font-normal leading-normal text-blue-gray-900 antialiased transition-colors duration-300 hover:text-blue-500">
+        <a
+          class="font-medium text-blue-gray-900 transition-colors hover:text-blue-500"
+          href="#"
+        >
+          Research Home
+        </a>
+      </li>
+    </ol>
+  </nav>
+
+
+<!-- stylesheet -->
+<link
+  rel="stylesheet"
+  href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
+/>
 <div class="bg-gray-50 dark:bg-black p-4 flex items-center justify-center mt-16">
     <div class="bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-800 p-4 rounded-xl border max-w-full">
-        <div class="flex justify-between">
+        <!-- <div class="flex justify-between">
             <div class="flex items-center">
 
                 <div class="ml-1.5 text-sm leading-tight">
@@ -9,10 +53,18 @@
                 </div>
             </div>
 
-        </div>
-        <p class="text-black dark:text-white block text-xl leading-snug mt-3 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, porro molestiae perferendis deserunt est quisquam quos fuga ut nam fugiat repellendus similique odit fugit, placeat explicabo! Doloremque distinctio neque quo quam architecto natus dignissimos, cumque ad molestias ab laborum, fugit cum velit autem, exercitationem quasi eaque debitis officiis necessitatibus optio?.</p>
+        </div> -->
+        <section class="py-4">
+      <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <div class="mb-12 space-y-2 text-center">
+              <h2 class="text-4xl font-bold text-gray-800 md:text-5xl dark:text-white"> Overview </h2>
+             
+          </div>
+      </div>
+  </section>
+        <p class="text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, porro molestiae perferendis deserunt est quisquam quos fuga ut nam fugiat repellendus similique odit fugit, placeat explicabo! Doloremque distinctio neque quo quam architecto natus dignissimos, cumque ad molestias ab laborum, fugit cum velit autem, exercitationem quasi eaque debitis officiis necessitatibus optio?.</p>
 
-        <img class="mt-2  object-cover aspect-video max-h-80   w-full rounded-2xl border border-gray-100 dark:border-gray-700" src="https://images.unsplash.com/photo-1501290741922-b56c0d0884af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzZWFyY2h8ZW58MHx8MHx8fDA%3D" alt="Research and Innovation" />
+        <img class="mt-2  object-cover aspect-video max-h-80   w-full rounded-2xl border border-gray-100 dark:border-gray-700" src="https://images.unsplash.com/photo-1501290741922-b56c0d0884af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVzZWFyY2h8ZW58MHx8MHx8fDA%3D" alt="Research and Innovation" loading="lazy" />
 
 
 
@@ -21,17 +73,17 @@
             <div class="flex items-center">
 
                 <div class="ml-1.5 text-sm leading-tight">
-                    <h2 class=" mb-6 block font-sans text-4xl font-medium tracking-normal text-gray-500 antialiased">Researchers are involved in research in one of the following thematic areas:</h2>
+                    <h2 class=" font-semibold text-lg text-gray-700 dark:text-teal-300">Researchers are involved in research in one of the following thematic areas:</h2>
 
                 </div>
             </div>
 
         </div>
-        <p class="text-black dark:text-white block text-xl leading-snug mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis id iste iure? Dolores labore quisquam deserunt reiciendis provident cumque commodi!.</p>
+        <p class="text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis id iste iure? Dolores labore quisquam deserunt reiciendis provident cumque commodi!.</p>
 
 
 
-        <ul class="text-gray-500  dark:text-white  text-xl leading-snug mt-3 list-item  list-disc list-outside ml-4">
+        <ul class=" text-gray-500 dark:text-gray-400 list-item  list-disc list-outside ml-4">
             <li>Synthesis and Evaluation of molecules with Pharmaceutical (Anti-HIV, Anti-malarial, Anti-Tumor, Anti-bacterial) , Agrochemical and Perfumery applications.</li>
             <li>Computer–aided design synthesis and evaluation of new bioactive molecules.</li>
             <li>Extraction, Isolation, Standardization, Formulation and Identification of herbal extracts/new molecules.</li>
@@ -40,17 +92,15 @@
             <li>Green Chemistry,Bioconversion</li>
         </ul>
 
-        <div class="flex justify-between mt-4">
-            <div class="flex items-center">
-
-                <div class="ml-1.5 text-sm leading-tight">
-                    <h2 class=" mb-6 block font-sans text-4xl font-medium tracking-normal text-gray-500 antialiased">CONSULTANCY</h2>
-
-                </div>
-            </div>
-
-        </div>
-        <ul class="text-gray-500  dark:text-white  text-xl leading-snug mt-3 list-item  list-disc list-outside ml-4">
+        <section class="py-4">
+      <div class="xl:container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <div class="mb-12 space-y-2 text-center">
+              <h2 class="text-4xl font-bold text-gray-800 md:text-5xl dark:text-white"> CONSULTANCY </h2>
+             
+          </div>
+      </div>
+  </section>
+        <ul class="text-gray-500  dark:text-white  text-xl leading-snug mt-3 list-itemtext  list-disc list-outside ml-4">
             <li>The college is offering consultancy services in the area of biotechnology</li>
             <li>During this year, we have successfully participated (along side private
                 enterprises) in an open bid to cleanse the highly polluted Lakes in Thane
@@ -116,8 +166,8 @@
 <section class="py-10 bg-gray-100 ">
     <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div class="grid items-center grid-cols-1 gap-y-8 lg:grid-cols-2 gap-x-16 xl:gap-x-24">
-            <div class="relative mb-12">
-                <img class="w-full rounded-md" src="https://media.istockphoto.com/id/185106846/photo/patented-stamp.webp?b=1&s=170667a&w=0&k=20&c=69Ivw8G1jajncRt_BKhdyRL-iBrIgAuU0kneRhVlBtY=" alt="" />
+            <div class="relative mb-12 max-w-5xl">
+                <img   class=" w-full  rounded-md" src="https://media.istockphoto.com/id/185106846/photo/patented-stamp.webp?b=1&s=170667a&w=0&k=20&c=69Ivw8G1jajncRt_BKhdyRL-iBrIgAuU0kneRhVlBtY=" alt="" />
 
                 <div class="absolute w-full max-w-xs px-4 -translate-x-1/2 sm:px-0 sm:max-w-sm left-1/2 -bottom-12">
                     <div class="overflow-hidden bg-white rounded">
@@ -129,7 +179,7 @@
                         </div>
                     </div>
                 </div>
-                
+
             </div>
 
             <div>
@@ -139,26 +189,26 @@
                     </svg>
                 </div> -->
                 <div class="flex justify-between">
-                <div class=" w-full max-w-xs px-4 m-4 sm:px-0 sm:max-w-sm ">
-                    <div class="overflow-hidden bg-white rounded">
-                        <div class="px-10 py-6">
-                             <div class="flex items-center">
-                                <p class="flex-shrink-0 text-3xl font-bold text-blue-600 sm:text-4xl">30+</p>
-                                <p class="pl-6 text-sm font-medium text-black sm:text-lg">Patent Granted</p>
+                    <div class=" w-full max-w-xs px-4 m-4 sm:px-0 sm:max-w-sm ">
+                        <div class="overflow-hidden bg-white rounded">
+                            <div class="px-10 py-6">
+                                <div class="flex items-center">
+                                    <p class="flex-shrink-0 text-3xl font-bold text-blue-600 sm:text-4xl">30+</p>
+                                    <p class="pl-6 text-sm font-medium text-black sm:text-lg">Patent Granted</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class=" w-full max-w-xs m-4 px-4 sm:px-0 sm:max-w-sm ">
-                    <div class="overflow-hidden bg-white rounded">
-                        <div class="px-10 py-6">
-                            <div class="flex items-center">
-                                <p class="flex-shrink-0 text-3xl font-bold text-blue-600 sm:text-4xl">82</p>
-                                <p class="pl-6 text-sm font-medium text-black sm:text-lg">Patent Published</p>
+                    <div class=" w-full max-w-xs m-4 px-4 sm:px-0 sm:max-w-sm ">
+                        <div class="overflow-hidden bg-white rounded">
+                            <div class="px-10 py-6">
+                                <div class="flex items-center">
+                                    <p class="flex-shrink-0 text-3xl font-bold text-blue-600 sm:text-4xl">82</p>
+                                    <p class="pl-6 text-sm font-medium text-black sm:text-lg">Patent Published</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 </div>
                 <h2 class="mt-10 text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">Patents</h2>
                 <p class="mt-6 text-lg leading-relaxed text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officia praesentium nulla in rerum magni..</p>
