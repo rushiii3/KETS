83232fae520cd6da799feec9c15199afe418facc
17SachinSingh <17sachinthakur@gmail.com>
2024-06-29 15:33:14 +0530

    sponsered research page of reserch tab added

---
+++ College/src/main/Components/Research_tab/sponsered_research.php
@@ -0,0 +1,146 @@
+ <div>
+     <div class="relative isolate flex flex-col overflow-hidden py-2 md:h-[25rem] h-[15rem] mx-auto w-full items-center justify-center">
+         <img src="../../../assests/college.png" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
+         <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-800 via-gray-900/10"></div>
+         <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-white/10"></div>
+         <div class="text-center z-1" data-aos="fade-up">
+             <h1 class="md:text-7xl text-5xl font-bold tracking-tight text-white sm:text-6xl text-center px-[20px]">
+                 Research and Innovation
+             </h1>
+         </div>
+
+     </div>
+
+ </div>
+
+ <nav class="bg-white border-gray-200 dark:bg-black">
+     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+         <a href="" class="flex items-center space-x-3 rtl:space-x-reverse    ">
+
+             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+         </a>
+         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+             <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false" id="menu-button">
+                 <span class="sr-only">Open main menu</span>
+                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                 </svg>
+             </button>
+         </div>
+         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
+                 <li>
+                     <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Research Home</a>
+                 </li>
+                 <li>
+                     <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Academic Research</a>
+                 </li>
+                 <li>
+                     <a href="#" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Sponsored Research</a>
+                 </li>
+                 <li>
+                     <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Achievements</a>
+                 </li>
+
+                 <li>
+                     <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Aviskar</a>
+                 </li>
+             </ul>
+         </div>
+     </div>
+ </nav>
+
+ <script>
+     document.getElementById('menu-button').addEventListener('click', function() {
+         var navbar = document.getElementById('navbar-cta');
+         navbar.classList.toggle('hidden');
+     });
+ </script>
+
+ <!-- Component: Flat breadcrumb with text & leading icon -->
+ <nav aria-label="Breadcrumb" class="pl-3 mt-5 dark:bg-black dark:text-white " id="Breadcrumb" data-aos="fade-right">
+     <ol class="flex items-stretch gap-2 list-none">
+         <li class="flex items-center gap-2">
+             <a href="#" class="flex max-w-[20ch] items-center gap-1 truncate whitespace-nowrap text-slate-700 transition-colors hover:text-emerald-500">
+                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" aria-hidden="true" aria-labelledby="title-01 description-01" role="link">
+                     <title id="title-01">Home</title>
+                     <desc id="description-01">
+                         Home button indicating the homepage of the website.
+                     </desc>
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                 </svg>
+                 <span class="hidden md:block">Home</span>
+             </a>
+             <svg xmlns="http://www.w3.org/2000/svg" class="flex-none w-4 h-4 transition-transform stroke-slate-700 md:rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" aria-hidden="true" aria-labelledby="title-02 description-02" role="graphics-symbol">
+                 <title id="title-02">Arrow</title>
+                 <desc id="description-02">
+                     Arrow icon that points to the next page in big screen resolution sizes
+                     and previous page in small screen resolution sizes.
+                 </desc>
+                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
+             </svg>
+         </li>
+         <li class="flex items-center flex-1 gap-2">
+             <a href="#" aria-current="page" class="pointer-events-none max-w-[20ch] truncate whitespace-nowrap text-slate-400">Sponsered Research</a>
+
+         </li>
+     </ol>
+ </nav>
+ <section>
+     <section class="bg-white dark:bg-black">
+         <section>
+             <div class="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
+                 <div class="flex w-full mx-auto text-left">
+                     <div class="relative inline-flex items-center mx-auto align-middle">
+                         <div class="text-center">
+                             <h1 class="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
+                                 Sponsered Research
+                             </h1>
+                             <p class="max-w-xl mx-auto mt-8 text-base leading-relaxed text-gray-500">Sponsored research at our college partners with top organizations to fund and drive innovative projects.</p>
+                             <div class="flex justify-center w-full max-w-2xl gap-2 mx-auto mt-10 border-2">
+                                 <div class="w-full">
+                                     <div class="overflow-x-auto">
+                                         <div class="p-5">
+                                             <div class="overflow-hidden">
+                                                 <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                                                     <thead>
+                                                         <tr>
+                                                             <th scope="col" class="px-6 py-3 text-center text-xl font-medium text-gray-900 uppercase dark:text-neutral-500" colspan="2">Research Assistance to Industries</th>
+                                                         </tr>
+                                                     </thead>
+                                                     <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                                                         <tr>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">S. H. Kelkar and Co. Ltd.</td>
+                                                             <td class="px-6 py-4  text-sm text-gray-800 dark:text-neutral-200">Development of new molecules for Perfumery and cosmetic applications. Design of simplified and green procedures for production of perfumery chemicals.</td>
+                                                         </tr>
+                                                         <tr>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">Ph D Research Students Science and Humanities</td>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">17</td>
+                                                         </tr>
+                                                         <tr>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">MSc (By Research) Awarded Science</td>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">09</td>
+                                                         </tr>
+                                                         <tr>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">PhD Awarded</td>
+                                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">10</td>
+                                                         </tr>
+                                                     </tbody>
+                                                 </table>
+                                             </div>
+                                         </div>
+                                     </div>
+                                 </div>
+                             </div>
+
+                         </div>
+                     </div>
+                 </div>
+         </section>
+
+     </section>
+
+
+
+ </section>
---
+++ College/src/main/Pages/Research_tab/sponsored_research.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="The college website">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V. G. </title>
+    <?php include('../../../library/library.php'); ?>
+    <?php include('../../../config/connect.php'); ?>
+</head>
+
+<body class=" mx-auto overflow-x-hidden dark:bg-black dark:text-white  ">
+    <?php include('../../Layouts/header2.php'); ?>
+    <?php include('../../Components/Research_tab/sponsered_research.php'); ?>
+    <?php include('../../Layouts/footer.php'); ?>
+
+</body>
+<?php include('../../../library/AOS.php'); ?>
+
+</html>
